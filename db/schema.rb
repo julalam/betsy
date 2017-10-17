e181817dd52249fe5da1feeadad4674d89e4e0ce
Rebecca Bergena <rbergena@gmail.com>
2017-10-17 16:57:42 -0700

    revert

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
+ActiveRecord::Schema.define(version: 20171017232857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,15 +21,6 @@
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-
-  create_table "orders", force: :cascade do |t|
-    t.string "status"
-    t.string "customer_name"
-    t.string "customer_email"
-    t.integer "cc_number"
-    t.date "cc_exipration"
-    t.integer "cc_ccv"
 
   create_table "order_items", force: :cascade do |t|
     t.integer "quantity"
