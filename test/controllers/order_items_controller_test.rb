a0537f93c0b2875db40c77d7e462e4fd64ca612f
Rebecca Bergena <rbergena@gmail.com>
2017-10-18 11:52:27 -0700

    order items controller and model

--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -4,4 +4,40 @@ describe OrderItemsController do
   # it "must be a real test" do
   #   flunk "Need real tests"
   # end
+  describe "index" do
+    it "succeeds when there are order items" do
+      OrderItem.count.must_be :>, 0, "No order items in the test fixtures"
+      get orderitems_path
+      must_respond_with :success
+    end
+
+    it "succeeds when there are no order items" do
+      OrderItem.destroy_all
+      get orderitems_path
+      must_respond_with :success
+    end
+  end
+  describe "destroy" do
+    it "succeeds for an extant order item ID" do
+      order_item_id = OrderItem.first.id
+
+      delete work_path(order_item_id)
+      must_redirect_to root_path
+
+      OrderItem.find_by(id: order_item_id).must_be_nil
+    end
+
+    # it "renders 404 not_found and does not update the DB for a bogus work ID" do
+    #   start_count = OrderItem.count
+    #
+    #   bogus_order_item_id = OrderItem.last.id + 1
+    #   delete work_path(bogus_order_item_id)
+    #   must_respond_with :not_found
+    #
+    #   Work.count.must_equal start_count
+    # end
+
+  end
+
+
 end
