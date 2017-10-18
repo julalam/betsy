16d0fedc228ad925a2ac6dfab0ff8bb4442b78a0
VirtualLindsey <tapircode@gmail.com>
2017-10-17 23:02:53 -0700

    creating routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get '/orders', to: 'orders#index', as: 'orders'
+    get '/orders/new', to: 'orders#new', as: 'new_order'
+    post '/orders', to: 'orders#create'
+    patch '/orders/:id', to: 'orders#update'
+
+    resources :orders
 end
