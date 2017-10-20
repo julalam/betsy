9cacb4d40bb888e8548c9644882f22c3fe34ec89
Eva Sawyer <esawyer25@gmail.com>
2017-10-20 11:00:53 -0700

    checked MainController tests

--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 describe MainController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    it "gets new and returns a success status for the new page" do
+      get root_path
+      must_respond_with :success
+    end
+
+    it "must work if there are no products in the database" do
+      Product.destroy_all
+      get root_path
+      must_respond_with :success
+    end
+
+    #TODO it "must work if there is between 0 and 6 Products"
+    #right now this will fail, but we decided we don't care.
+    #it will be able to deploy with an empty database,
+    #and then we will seed the database to have more thant
+    #6 products.
+
+  end
 end
