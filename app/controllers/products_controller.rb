2c4032231de8679d7cbe3542b339945c63237d94
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-10-20 18:36:04 -0700

    Add view for both statuses of products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -35,7 +35,6 @@ class ProductsController < ApplicationController
     if @product.save
       flash[:status] = :success
       flash[:result_text] = "Successfully updated #{@product.name}, ID number #{@product.id}"
-      # redirect_to product_path(@product)
       redirect_to merchant_products_path(session[:merchant_id])
     else
       flash[:status] = :failure
@@ -62,7 +61,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :price, :stock, :description, :image_url)
+    params.require(:product).permit(:name, :price, :stock, :retired, :description, :image_url)
   end
 end
 
