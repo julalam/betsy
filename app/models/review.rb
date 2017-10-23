5392335ae7f3a555ecaf2df56dacb145b05cc57c
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-10-23 11:51:03 -0700

    Add tests for review validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,5 @@ class Review < ApplicationRecord
   belongs_to :product
 
   validates :rating, presence: true
-  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5, message: "Please enter a rating from 1 to 5" }
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5, message: "Please enter a rating from 1 to 5" }
 end
--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -12,4 +12,26 @@ describe Review do
     end
   end
 
+  describe "validations" do
+    it "requires rating to be created" do
+      review = Review.new
+      review.wont_be :valid?
+    end
+
+    it "can be created with valid rating" do
+      review = Review.new(rating: 4, product: products(:one))
+      review.must_be :valid?
+    end
+
+    it "requires rating to be 1-5" do
+      review = Review.new(rating: 10, product: products(:one))
+      review.wont_be :valid?
+    end
+
+    it "rating has to be an integer" do
+      review = Review.new(rating: 4.5, product: products(:one))
+      review.wont_be :valid?
+    end
+  end
+
 end
