98adcb75fd28847062c3be86ae02a9a43d6a2a3b
Eva Sawyer <esawyer25@gmail.com>
2017-10-23 16:07:47 -0700

    worked on layout for order_item summary

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,8 +3,8 @@ class OrderItem < ApplicationRecord
   belongs_to :product
 
   # Make a method for this and put it in _orderitem_summary
-  # def total_cost(cost)
-  #   cost = @quantity * cost
-  # return result
-  # end
+  def self.total_cost(order_item)
+    result = order_item.quantity * order_item.product.price
+    return result
+  end
 end
