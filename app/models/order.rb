6999806e59c99ff6ce6c0698187812a95706a139
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-10-23 16:53:54 -0700

    Add bestseller method to product

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,6 @@ class Order < ApplicationRecord
   # :customer_name, :customer_email, :customer_address,
   # :cc_number,:cc_expiration, :cc_ccv,
   # :zip_code, presence: true
+
+  has_many :products, through: :order_items 
 end
