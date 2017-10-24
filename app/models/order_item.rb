5aa56c243e7eb1e810fbe54f6d458ddba69eeb4a
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-10-24 12:38:06 -0700

    Add tests for custom methods for totals

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -4,27 +4,14 @@ class OrderItem < ApplicationRecord
 
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
 
-  # Make a method for this and put it in _orderitem_summary
-  # def total_cost(cost)
-  #   cost = @quantity * cost
-  # return result
-  # end
-
-  def total
-    sum = product.price * quantity
-    return sum
-  end
-
   def self.subtotal(order_item)
     result = order_item.quantity * order_item.product.price
     return result
   end
 
-
   def self.total_cost(collection)
     total = 0
     collection.each do |order_item|
-      # total += order_item.quantity * order_item.product.price
       total += subtotal(order_item)
     end
     return total
--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -15,33 +15,19 @@ describe OrderItem do
       o.must_respond_to :product
       o.wont_be :valid?
       o.errors.messages.must_include :product
-  let(:order_item_1){ OrderItem.create!(product_id: products(:one).id, order_id: orders(:one).id, quantity: 3)}
-  describe "relations" do
-    it "has an order" do
-      order_item_1.must_respond_to :order
-      order_item_1.order.must_be_kind_of Order
     end
 
-    it "has a product" do
-      order_item_1.must_respond_to :product
-      order_item_1.product.must_be_kind_of Product
-    end
-  end
 
-  describe "total cost" do
-    let(:order_item_2){ OrderItem.create!(product_id: products(:two).id, order_id: orders(:one).id, quantity: 3)}
 
-    it "given one order_item, returns the cost of that order_item" do
-      OrderItem.total_cost(order_item_1).must_equal 6
-    end
-
-    it "given a collection of order_items, returns the cost of those items" do
-      OrderItem.destroy_all
-      order_item_1
-      order_item_2
-      order_items = OrderItem.all
-      OrderItem.total_cost(order_items).must_equal 15
-    end
+    # it "has an order" do
+    #   order_item_1.must_respond_to :order
+    #   order_item_1.order.must_be_kind_of Order
+    # end
+    #
+    # it "has a product" do
+    #   order_item_1.must_respond_to :product
+    #   order_item_1.product.must_be_kind_of Product
+    # end
   end
 
   describe "validations" do
@@ -79,14 +65,21 @@ describe OrderItem do
       o.errors.messages.must_include :quantity
       o.errors.messages.values.first.must_include "must be greater than 0"
     end
-
   end
 
   describe "custom methods" do
-    describe "total method" do
-      # quantity for oi1 is 2 and price for product one is 2
-      it "returns the subtotal (i.e., price * quantity)" do
-        order_items(:oi1).total.must_equal 4
+    describe "subtotal and total_cost" do
+      before do
+        @order_item = OrderItem.new(quantity: 3, order: orders(:one), product: products(:one))
+        @other_order_item = OrderItem.new(quantity: 2, order: orders(:one), product: products(:two))
+      end
+
+      it "returns the right subtotal" do
+        OrderItem.subtotal(@order_item).must_equal @order_item.quantity * @order_item.product.price
+      end
+
+      it "returns the right total cost" do
+        OrderItem.total_cost([@order_item, @other_order_item]).must_equal @order_item.quantity * @order_item.product.price + @other_order_item.quantity * @other_order_item.product.price
       end
     end
   end
