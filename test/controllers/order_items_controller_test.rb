74770975b8c881dc74b06c7bedeeffea83dbd276
VirtualLindsey <tapircode@gmail.com>
2017-10-23 21:03:10 -0700

    resolivng merge conflicts

--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -1,44 +1,77 @@
 require "test_helper"
 
 describe OrderItemsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
-  describe "index" do
-    it "succeeds when there are order items" do
-      OrderItem.count.must_be :>, 0, "No order items in the test fixtures"
-      get orderitems_path
-      must_respond_with :success
+  describe "require login methods" do
+    before do
+      @merchant = merchants(:eva)
+      login(@merchant)
     end
 
-    it "succeeds when there are no order items" do
-      OrderItem.destroy_all
-      get orderitems_path
-      must_respond_with :success
+    describe "new" do
+      it "returns success" do
+        get new_order_item_path
+        must_respond_with :success
+      end
     end
+
+    describe "index" do
+      it "succeeds when there are order items" do
+        get order_items_path
+        must_respond_with :success
+      end
+
+      it "succeeds when there are no order items" do
+        OrderItem.destroy_all
+        get order_items_path
+        must_respond_with :success
+      end
+    end
+
+    describe "update" do
+      it "updates an existing order" do
+        order = OrderItem.first
+
+        order_data = {
+          order_item: {
+            order_id: order.order_id,
+            product_id: order.product_id,
+            quantity: order.quantity - 1
+          }
+        }
+
+        order.update_attributes(product_data[:order_item])
+        order.must_be :valid?
+
+        patch order_path(order), params: order_data
+        must_redirect_to redirect_back
+
+      end
+    end
+
+    describe "create" do
+      it "creates a new order item with valid data" do
+        order_data = {
+          order_item: {
+            order_id: 2,
+            product_id: 1,
+            quantity: 3
+          }
+        }
+      new_order_item = OrderItem.new(order_data[:order_item])
+      post order_items_path, params: order_data
+      must_redirect_to order_items_path
+      end
+    end
+
+    describe "destroy" do
+      it "succeeds for an extant order item ID" do
+        order_item_id = OrderItem.first
+
+        delete order_item_path(order_item_id)
+        OrderItem.find(order_item_id.id).must_be_nil
+        must_redirect_to root_path
+      end
+    end
+
   end
-
-  describe "destroy" do
-    it "succeeds for an extant order item ID" do
-      order_item_id = OrderItem.first.id
-
-      delete order_item_path(order_item_id)
-      must_redirect_to root_path
-
-      OrderItem.find_by(id: order_item_id).must_be_nil
-    end
-
-    # it "renders 404 not_found and does not update the DB for a bogus order_item ID" do
-    #   start_count = OrderItem.count
-    #
-    #   bogus_order_item_id = OrderItem.last.id + 1
-    #   delete order_item_path(bogus_order_item_id)
-    #   must_respond_with :not_found
-    #
-    #   order_item.count.must_equal start_count
-    # end
-
-  end
-
-
 end
