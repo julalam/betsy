af4f46858405227133770fe322513d1a95e3e73f
Rebecca Bergena <rbergena@gmail.com>
2017-10-24 09:08:37 -0700

    merchant and order items model tests and custom methods

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,9 +2,17 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
   # Make a method for this and put it in _orderitem_summary
   # def total_cost(cost)
   #   cost = @quantity * cost
   # return result
   # end
+
+  def total
+    sum = product.price * quantity
+    return sum
+  end
+
 end
--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -1,23 +1,68 @@
 require "test_helper"
 
 describe OrderItem do
-  # let(:order_item) { OrderItem.new }
-  #
-  # it "must be valid" do
-  #   value(order_item).must_be :valid?
-  # end
 
   describe "relations" do
-    it "has an order" do
-      o = orderitems(:one)
+    it "belongs to an order and creates an error message if no order is given" do
+      o = OrderItem.new(quantity: 2, product: products(:one))
       o.must_respond_to :order
-      o.order.must_be_kind_of Order
+      o.wont_be :valid?
+      o.errors.messages.must_include :order
     end
 
-    it "has a product" do
-      o = orderitems(:one)
+    it "belongs to a product and creates an error message if no product is given" do
+      o = OrderItem.new(quantity: 2, order: orders(:one))
       o.must_respond_to :product
-      o.order.must_be_kind_of Product
+      o.wont_be :valid?
+      o.errors.messages.must_include :product
     end
   end
+
+  describe "validations" do
+
+    it "can be created with all required fields (including a positive integer for quantity)" do
+      o = OrderItem.new(quantity: 3, order: orders(:one), product: products(:one))
+      o.must_be :valid?
+    end
+
+    it "cannot be created without a quantity" do
+      o = OrderItem.new( order: orders(:one), product: products(:one))
+      o.wont_be :valid?
+      o.errors.messages.must_include :quantity
+      o.errors.messages.values.first.must_include "can't be blank"
+    end
+
+    it "cannot be created with a float" do
+      o = OrderItem.new(quantity: 3.3, order: orders(:one), product: products(:one))
+      o.wont_be :valid?
+      o.errors.messages.must_include :quantity
+      o.errors.messages.values.first.must_include "must be an integer"
+    end
+
+
+    it "cannot be created with a quantity of 0" do
+      o = OrderItem.new(quantity: 0, order: orders(:one), product: products(:one))
+      o.wont_be :valid?
+      o.errors.messages.must_include :quantity
+      o.errors.messages.values.first.must_include "must be greater than 0"
+    end
+
+    it "cannot be created with a negative quantity" do
+      o = OrderItem.new(quantity: -1, order: orders(:one), product: products(:one))
+      o.wont_be :valid?
+      o.errors.messages.must_include :quantity
+      o.errors.messages.values.first.must_include "must be greater than 0"
+    end
+
+  end
+
+  describe "custom methods" do
+    describe "total method" do
+      # quantity for oi1 is 2 and price for product one is 2
+      it "returns the subtotal (i.e., price * quantity)" do
+        order_items(:oi1).total.must_equal 4
+      end
+    end
+  end
+
 end
