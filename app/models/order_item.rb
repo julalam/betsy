84f776c56b958c9a54790a2d9a09c931e0ff5705
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-10-23 22:43:45 -0700

    Add styles to shopping cart

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -5,6 +5,9 @@ class OrderItem < ApplicationRecord
   # Make a method for this and put it in _orderitem_summary
   def self.total_cost(order_item)
     result = order_item.quantity * order_item.product.price
+    if result < 50
+      result += 10
+    end
     return result
   end
 end
