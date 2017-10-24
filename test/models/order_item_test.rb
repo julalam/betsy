b43a78bc5fced5b389a8f089be2060a317c7f2fc
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-10-24 12:43:47 -0700

    Complete all model tests for order_item

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -3,34 +3,34 @@ require "test_helper"
 describe OrderItem do
 
   describe "relations" do
-    it "belongs to an order and creates an error message if no order is given" do
+    before do
+      @order_item = OrderItem.new(quantity: 3, order: orders(:one), product: products(:one))
+    end
+
+    it "has an order" do
+      @order_item.must_respond_to :order
+      @order_item.order.must_be_kind_of Order
+    end
+
+    it "has a product" do
+      @order_item.must_respond_to :product
+      @order_item.product.must_be_kind_of Product
+    end
+
+  end
+
+  describe "validations" do
+    it "creates an error message if no order is given" do
       o = OrderItem.new(quantity: 2, product: products(:one))
-      o.must_respond_to :order
       o.wont_be :valid?
       o.errors.messages.must_include :order
     end
 
-    it "belongs to a product and creates an error message if no product is given" do
+    it "creates an error message if no product is given" do
       o = OrderItem.new(quantity: 2, order: orders(:one))
-      o.must_respond_to :product
       o.wont_be :valid?
       o.errors.messages.must_include :product
     end
-
-
-
-    # it "has an order" do
-    #   order_item_1.must_respond_to :order
-    #   order_item_1.order.must_be_kind_of Order
-    # end
-    #
-    # it "has a product" do
-    #   order_item_1.must_respond_to :product
-    #   order_item_1.product.must_be_kind_of Product
-    # end
-  end
-
-  describe "validations" do
 
     it "can be created with all required fields (including a positive integer for quantity)" do
       o = OrderItem.new(quantity: 3, order: orders(:one), product: products(:one))
