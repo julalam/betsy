fa93b571535aa43b6563b9e21f6d1a6e9866422e
Iuliia Chikulaeva <julia.chikulaeva@gmail.com>
2017-10-26 09:43:27 -0700

    Change create and new in reviews

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -31,7 +31,8 @@ describe ReviewsController do
     it "renders bad_request and does not update the DB for bogus data" do
       review_data = {
         review: {
-          product_id: nil
+          product_id: products(:one).id,
+          rating: nil
         }
       }
       start_count = Review.count
@@ -41,6 +42,23 @@ describe ReviewsController do
       must_respond_with :bad_request
       Review.count.must_equal start_count
     end
-  end
 
+    describe "restrict_merchants" do
+      before do
+        @owner = merchants(:eva)
+        @user = merchants(:emma)
+        login(@owner)
+        @product = @owner.products.first
+        @right_review = Review.new(product_id: @product.id, rating: 5)
+        @wrong_review = Review.new(product_id: @product.id, rating: 3)
+      end
+      it "let to not owner review the product" do
+        @xcontroller.send(:restrict_merchants, @right_review).must_be true
+      end
+
+      it "do not allow the owner review their products" do
+        @xcontroller.send(:restrict_merchants, @wrong_review).must_be false
+      end
+    end
+  end
 end
